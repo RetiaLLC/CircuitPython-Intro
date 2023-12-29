6801327ba6226101ecb2b6f632ceb1f678ea02e5
CRImier <crimier@yandex.ru>
2023-12-29 12:21:45 +0100

    Button pins

--- Files/Examples/Examples_Button_Press.py
+++ Files/Examples/Examples_Button_Press.py
@@ -1,7 +1,13 @@
 import time, board, digitalio
 
 button = digitalio.DigitalInOut(board.IO18) #Down Button
+# Down button - IO18
+# Up button - IO9
+# Left button - IO11
+# Right button - IO7
 button.switch_to_input(digitalio.Pull.UP)
+
+
 
 while True:
     if button.value == False:
